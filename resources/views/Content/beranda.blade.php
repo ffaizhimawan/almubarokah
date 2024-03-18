69529b25053aff04398d18bfb00f08ea1c51639a
Faiz Himawan <faizhimawan26@gmail.com>
2024-03-18 11:56:27 +0700

    first commit

---
+++ resources/views/Content/beranda.blade.php
@@ -0,0 +1,14 @@
+@extends('Layout.layout')
+
+@section('content')
+    @include('Component.navbar')
+    <div class="wrapper">
+      <div class="container main">
+        <div>
+          background
+          <img class="gambarSekolah" src="img/school.jpg" alt="sekolah">
+        </div>
+      </div>
+    </div>
+    
+@endsection
